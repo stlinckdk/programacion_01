f8a0e59afdb84030c050fbcc73da3a46dd7bd05a
Alumno07 J901 <a07j901@campustls.local>
2019-01-17 20:02:25 -0500

    agrego nuevo js

--- main.js
+++ main.js
@@ -3,10 +3,23 @@ document.addEventListener('DOMContentLoaded', function(){
   // botonComprar.addEventListener("click", function() {
   //   alert("Gracias por tu compra");
   // })
+  // const form = document.querySelectorAll(".form")[0];
+  // form.addEventListener("submit", function() {
+  //   const cantidad = document.querySelectorAll(".cantidad")[0].value;
+  //   const producto = document.querySelectorAll(".producto")[0];
+  //   const notas = document.querySelectorAll(".notas")[0].value;
+  //
+  //
+  //   alert(`gracias por tu compra de ${cantidad} de ${producto.options[producto.selectedIndex].text} con las siguientes notas ${notas}`);
+
   const form = document.querySelectorAll(".form")[0];
-  form.addEventListener("submit", function() {
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
     const cantidad = document.querySelectorAll(".cantidad")[0].value;
-    const lista = document.querySelectorAll(".lista")[0].value;
-    alert(`Gracias por comprar ${cantidad} ${lista}`);
+    const producto = document.querySelectorAll(".producto")[0];
+    const notas = document.querySelectorAll(".notas")[0].value;
+    const mensaje = `gracias por tu compra de ${cantidad} de ${producto.options[producto.selectedIndex].text} con las siguientes notas ${notas}`;
+    const mensajeTexto = document.querySelectorAll(".mensaje_texto")[0];
+    mensajeTexto.innerHTML = mensaje;
   })
 });
